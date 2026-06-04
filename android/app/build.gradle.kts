0faef99f5ddc00b094bf14d7e82fcdd0b867924c
topicmishaialosa-ops <topicmishaialosa-ops@users.noreply.github.com>
2026-06-04 17:00:30 +0500

    Bump version to 1.4.14

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.mp3party.downloader"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.4.13"
+        versionCode = 14
+        versionName = "1.4.14"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86", "x86_64")
         }
